4d42b28b9c237baa57efc047b65ced918e73d4be
Bhavya022 <dnd88778@gmail.com>
2023-09-28 19:02:20 +0530

    doneeeee

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('travel', 'root', 'bhavya22', {
-  host: 'localhost',
-  dialect: 'mysql', // or 'postgresql', 'sqlite', etc.
-});
-
-module.exports = sequelize;
